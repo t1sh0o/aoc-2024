deb96d9d9d63f4b13ed491e7165b9f4f590c1b65
Tihomir Tonov <tihomir.tonov@ampeco.global>
2024-12-05 22:20:17 +0200

    Add solution for day 3 puzzle 2

--- src/day3/mod.rs
+++ src/day3/mod.rs
@@ -17,7 +17,29 @@ pub fn puzzle1(run_sample: bool) -> i32 {
     valid.into_iter().sum()
 }
 pub fn puzzle2(run_sample: bool) -> i32 {
-    let cnt = read_file(2, None, run_sample);
+    let cnt = read_file(3, None, run_sample);
+    let mut valid = Vec::new();
+    let mut _do = true;
 
-    0
+    let re = Regex::new(r"((do\(\))|(don\'t\(\))|mul\((\d{1,3}),(\d{1,3})\))").unwrap();
+
+    for cap in re.captures_iter(cnt.as_str()) {
+        if let Some(_) = &cap.get(2) {
+            _do = true;
+        } else if let Some(_) = &cap.get(3) {
+            _do = false; 
+        } else if _do == true {
+            let x = &cap.get(4).unwrap();
+            let y = &cap.get(5).unwrap();
+
+            let x = x.as_str().parse::<i32>().unwrap();
+            let y = y.as_str().parse::<i32>().unwrap();
+
+            valid.push(x * y);
+        }
+    }
+    
+    valid.into_iter().sum()
 }
+
+
